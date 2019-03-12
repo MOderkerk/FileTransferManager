a4889273a2b1053f0659e2881d1e25e86ff77614
Marco Oderkerk <github@oderkerk.de>
2019-03-12 21:42:05 +0100

    Project init

---
+++ HELP.md
@@ -0,0 +1,8 @@
+# Getting Started
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Building a Hypermedia-Driven RESTful Web Service](https://spring.io/guides/gs/rest-hateoas/)
+* [Building a RESTful Web Service with Spring Boot Actuator](https://spring.io/guides/gs/actuator-service/)
+
---
+++ src/main/java/de/oderkerk/tools/filetransfermanager/FiletransfermanagerApplication.java
@@ -0,0 +1,13 @@
+package de.oderkerk.tools.filetransfermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletransfermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiletransfermanagerApplication.class, args);
+	}
+
+}
